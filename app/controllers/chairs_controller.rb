07fe4e7ced83410e3d098a4bc212b2bf3aae6d84
jordanoboyle <jmcmcode@gmail.com>
2024-05-14 00:16:55 -0600

    displaying both first and all of Chair API local

--- app/controllers/chairs_controller.rb
+++ app/controllers/chairs_controller.rb
@@ -1,5 +1,14 @@
 class ChairsController < ApplicationController
   def information
-    
+    render plain: " default"
+  end
+
+  def first
+    chair = Chair.first
+    render json: {
+      chair.id,
+      chair.weight,
+      
+    }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "/chair_model"  => "chairs#first"
   get "/chair_information" => "chairs#information"
 
   # Defines the root path route ("/")
